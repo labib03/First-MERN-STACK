46e1d516c33d55bc86ecb16c4a3287c6195c0898
l_asrdn <ansorudinlabib1@gmail.com>
2022-09-07 16:13:49 +0700

    install cors and init cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const workoutRoutes = require("./routes/workout");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // init
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
 // midleware
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
