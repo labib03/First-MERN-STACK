528aba9d5c9b6ea120c1dfab2ea60ef80d560172
l_asrdn <ansorudinlabib1@gmail.com>
2022-09-07 16:14:48 +0700

    fetching data from db & show in UI

---
+++ src/components/WorkoutDetail/WorkoutDetail.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function WorkoutDetail(props) {
+  const { workout } = props;
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg):</strong> {workout.load}
+      </p>
+      <p>
+        <strong>Reps:</strong> {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+}
+
+export default WorkoutDetail;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { WorkoutDetail } from "../../components";
 
 function Home() {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    async function getWorkouts() {
+      const response = await axios.get("http://localhost:4000/api/workout");
+      setWorkouts(response?.data);
+    }
+
+    getWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h3>Home</h3>
+      <div className="workouts">
+        {workouts &&
+          workouts?.data?.map((workout) => (
+            <WorkoutDetail key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 }
